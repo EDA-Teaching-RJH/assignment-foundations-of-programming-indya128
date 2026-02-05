e1bc1549d262e8b71f51aa995a76831ea0cc1103
indya128 <ib13@kent.ac.uk>
2026-02-05 15:16:57 +0000

    part 2

--- fleet_manager.py
+++ fleet_manager.py
@@ -4,3 +4,10 @@ def init_database():
     divisions = ["Command", "Command", "Operations", "sciences", "Security"]
     ids = [101,102,103,104,105]
     return names, ranks, divisions
+
+def display_menu(student_name)
+    print(f"\nLogged in as {student_name}")
+    print("--- FLEET MANAGEMENT SYSTEM ---")
+    print("display roster\n2. add crew member\n3. remove crew member\n4. update rank")
+    print("5. search crew member\n6.filter by divison\n7. calculate payroll\n8. count officers\n9 exit")
+    
